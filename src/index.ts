44e8724148a22c68d613828a81f605e26f7fd4e0
Alex Moreton <lxmrtn7@gmail.com>
2026-09-15 10:28:07 +0100

    Add origin + build upstream and console log it out

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@
 import { createServer } from "node:http";
 
 const PORT = 3000;
+const ORIGIN = "http://dummyjson.com";
 
-const server = createServer((_req, res) => {
+const server = createServer((req, res) => {
+  const upstreamUrl = new URL(req.url ?? "/", ORIGIN);
+  console.log(`${req.method} - ${req.url} - ${upstreamUrl.href}`);
   res.writeHead(200);
   res.end("OK\n");
 });
